23ee65590d186cd7fed9580f081798dc67faad9a
Isaac Cook <isaac@simpload.com>
2014-12-08 09:14:55 -0600

    Use more compact logging

--- lincoln/__init__.py
+++ lincoln/__init__.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import yaml
+import logging
 
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask, current_app
@@ -26,6 +28,17 @@ def create_app(log_level="INFO", config="config.yml"):
     # inject all the yaml configs
     app.config.update(config_vars)
     db.init_app(app)
+
+    del app.logger.handlers[0]
+    app.logger.setLevel(logging.NOTSET)
+    log_format = logging.Formatter('%(asctime)s [%(name)s] [%(levelname)s]: %(message)s')
+    log_level = getattr(logging, str(log_level), app.config.get('log_level', "INFO"))
+
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(log_format)
+    logger.addHandler(handler)
 
     app.rpc_connection = Proxy(
         "http://{0}:{1}@{2}:{3}/"
